178444320daab18021fa434e174f035b7759c517
wowbroforce <wowbroforce@gmail.com>
2017-04-14 15:08:37 +0300

    helper methods added

---
+++ Associate/Classes/Associate.swift
@@ -0,0 +1,36 @@
+//
+//  Associate.swift
+//  Pods
+//
+//  Created by Prokhor Kharchenko on 4/12/17.
+//
+//
+
+import Foundation
+
+public func associate(value: Any, with object: Any, by key: UnsafeRawPointer, policy: objc_AssociationPolicy = .OBJC_ASSOCIATION_RETAIN_NONATOMIC) {
+    objc_sync_enter(object)
+    defer {
+        objc_sync_exit(object)
+    }
+    objc_setAssociatedObject(object, key, value, policy)
+}
+
+public func associated<T: Associable>(with object: Any, by key: UnsafeRawPointer) -> T {
+    objc_sync_enter(object)
+    defer {
+        objc_sync_exit(object)
+    }
+    guard let value: T = objc_getAssociatedObject(object, key) as? T else {
+        let newValue = T.default()
+        objc_setAssociatedObject(object, key, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        return newValue as! T
+    }
+    return value
+}
+
+public protocol Associable {
+    associatedtype Element
+    
+    static func `default`() -> Element
+}
--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -4,26 +4,63 @@ import Associate
 
 class Tests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testCrossThreadAccess() {
+        DispatchQueue(label: "thread 1").asyncAfter(deadline: DispatchTime.now() + 0.01) {
+            for _ in 0..<10 {
+                objc_sync_enter(self)
+                defer { objc_sync_exit(self) }
+                self.counter.value = 0
+            }
+        }
+        DispatchQueue(label: "thread 2").async {
+            for _ in 0..<10 {
+                objc_sync_enter(self)
+                defer { objc_sync_exit(self) }
+                self.counter.value = 0
+            }
+        }
+        DispatchQueue(label: "thread 3").async {
+            for _ in 0..<10 {
+                objc_sync_enter(self)
+                defer { objc_sync_exit(self) }
+                self.counter.value = 0
+            }
+        }
+        
+        let exp = expectation(description: "-")
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
+            exp.fulfill()
+        }
+        
+        waitForExpectations(timeout: 2, handler: nil)
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
+}
+
+extension XCTestCase {
+    static var counterKey: Void?
+    var counter: Foo {
+        get {
+            return associated(with: self, by: &XCTestCase.counterKey)
+        }
+        set {
+            associate(value: newValue, with: self, by: &XCTestCase.counterKey)
         }
     }
+}
+
+class Foo: Associable {
+    let testCase: XCTestCase?
+    var value: Int = 0
     
+    init(testCase: XCTestCase? = nil) {
+        self.testCase = testCase
+    }
+    static func `default`() -> Foo {
+        return Foo()
+    }
+
+    deinit {
+        XCTFail()
+    }
 }
